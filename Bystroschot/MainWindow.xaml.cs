96eae52419787de0de3e236aeaff83c7099a69b0
EseniyaIvanovna <eseniamizhutina@yandex.ru>
2023-12-13 19:08:44 +0300

    ***

--- Bystroschot/MainWindow.xaml.cs
+++ Bystroschot/MainWindow.xaml.cs
@@ -370,7 +370,7 @@ namespace Bystroschot
             //метод для кнопки назад
             void GoBack(object sender1, RoutedEventArgs e1)
             {
-                if (idx == len-1)
+                if (idx == len - 1)
                 {
                     ShowFormula.Content = "Показать формулу";
                     ShowFormula.Click += ShowContent;
@@ -384,24 +384,19 @@ namespace Bystroschot
                 }
             }
             void ShowContent(object sender1, RoutedEventArgs e1)
-            {               
+            {
                 if (idx < len) //предусматривается, что все файлы тестов для разных вариантов будут одной длины
                 {
                     idx++;
                     PrintFormula(grid, idx);
-                    if (idx == len-1)
+                    if (idx == len - 1)
                     {
                         ShowFormula.Content = "Завершить тест";
                         ShowFormula.Click -= ShowContent;
                         ShowFormula.Click += GoHome;
                     }
-                    else
-                    {
-                        ShowFormula.Content = "Показать формулу";
-                        ShowFormula.Click += ShowContent;
-                    }
                 }
-               
+
             }
         }
         private void PrintFormula(Grid grid, int index)
@@ -477,7 +472,6 @@ namespace Bystroschot
         }
         public void Restore()
         {            
-<<<<<<< HEAD
             string[] strok = File.ReadAllLines("..\\Debug\\files\\history.xml");
             if (strok.Length != 0)
             {
@@ -486,18 +480,8 @@ namespace Bystroschot
                     var xml = new XmlSerializer(typeof(List<User>), new Type[] { typeof(User) });
                     users = (List<User>)xml.Deserialize(file);
                 }
-            }                       
-=======
-            //string[] strok = File.ReadAllLines("history.xml");
-            //if (strok.Length != 0)
-            //{
-            //    using (var file = new FileStream("history.xml", FileMode.OpenOrCreate))
-            //    {
-            //        var xml = new XmlSerializer(typeof(List<User>), new Type[] { typeof(User) });
-            //        users = (List<User>)xml.Deserialize(file);
-            //    }
-            //}                       
->>>>>>> ac3b4638288a57e35c4c5d31bcc5d672a074ba43
+            }                      
+
         }
     }
     
